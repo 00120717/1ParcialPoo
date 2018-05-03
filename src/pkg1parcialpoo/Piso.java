7df9e35cc4aa1eed800252217d1600a4a02a2784
Marvin  Ramirez <Marvin  Ramirez@ZoniaGonzalez>
2018-05-03 11:29:57 -0600

    Se agrego clase Piso

---
+++ src/pkg1parcialpoo/Piso.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1parcialpoo;
+
+/**
+ *
+ * @author Marvin Ramirez
+ */
+public class Piso {
+    private char idPiso;
+    private boolean estado;
+
+    
+    public Piso(char idPiso, boolean estado) {
+        this.idPiso = idPiso;
+        this.estado = estado;
+    }
+    
+    public char getIdPiso() {
+        return idPiso;
+    }
+
+    public void setIdPiso(char idPiso) {
+        this.idPiso = idPiso;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+}
