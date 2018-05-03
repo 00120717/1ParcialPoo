8b6eed2606ca69e0c0b2147389fd3dab37d66501
MMont <MMont@LAPTOP-KCJJ3911>
2018-05-03 11:11:42 -0600

    Se agregaron metodos y atributos a la clase NumCorrelativo

--- src/pkg1parcialpoo/NumCorrelativo.java
+++ src/pkg1parcialpoo/NumCorrelativo.java
@@ -2,8 +2,30 @@ package pkg1parcialpoo;
 
 /**
  *
- * @author MMont
+ * @author 00161617
  */
 public class NumCorrelativo {
+    private Piso piso;
+    private int numHabitacion;
+
+    public Piso getPiso() {
+        return piso;
+    }
+
+    public void setPiso(Piso piso) {
+        this.piso = piso;
+    }
+
+    public int getNumHabitacion() {
+        return numHabitacion;
+    }
+
+    public void setNumHabitacion(int numHabitacion) {
+        this.numHabitacion = numHabitacion;
+    }
+    
+    public boolean verificarEstado(){
+        return false;
+    }
     
 }
