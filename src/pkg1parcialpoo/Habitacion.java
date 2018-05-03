1610f31260a134dbbdbd5e4966ae93353e5c3d87
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-05-03 11:33:16 -0600

    Se agrego los atributos Numcorrelativo,precio y estado. se agregaron los  metodos set, get y verfiestado

--- src/pkg1parcialpoo/Habitacion.java
+++ src/pkg1parcialpoo/Habitacion.java
@@ -10,5 +10,41 @@ package pkg1parcialpoo;
  * @author Oscar Lovato
  */
 public class Habitacion {
-    
+    private NumCorrelativo idHabitacion;
+    private Precio precio;
+    private boolean estado;
+
+    public Habitacion() {
+    }
+
+    public NumCorrelativo getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(NumCorrelativo idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public Precio getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Precio precio) {
+        this.precio = precio;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    NumCorrelativo verifica=new NumCorrelativo();
+    public boolean verfiEstado(){
+        if(verifica.verificarEstado()){
+            return true;
+        }
+        return false;
+    }
 }
