90c8a09cc971a63a59424b5c0e81b53d3828e6e9
MMont <MMont@LAPTOP-KCJJ3911>
2018-05-03 11:13:53 -0600

    Se agrego clase Precio con atributos y metodos

---
+++ src/pkg1parcialpoo/Precio.java
@@ -0,0 +1,26 @@
+
+package pkg1parcialpoo;
+
+/**
+ *
+ * @author 00161617
+ */
+public class Precio {
+    
+    double precioBase;
+    
+    public Precio(){
+    }
+    public Precio (double precio){
+        precioBase = precio;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+    
+}
