9aad001483312545589aaf79bbee164356670969
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-05-03 13:54:19 -0600

    Se agregaron los atributos idReservacion, persona, habitacion, precio y paquete y metodos set y get

--- src/pkg1parcialpoo/Reservacion.java
+++ src/pkg1parcialpoo/Reservacion.java
@@ -10,11 +10,61 @@ package pkg1parcialpoo;
  * @author R.Fuentes
  */
 public class Reservacion {
+    public int idReservacion;
     public Persona huesped;
     public Habitacion habitacion;
     public Precio precio;
     public Paquete paquete;
     public int NumDias;
+    public Fecha fecha;
+
+    public int getIdReservacion() {
+        return idReservacion;
+    }
+
+    public void setIdReservacion(int idReservacion) {
+        this.idReservacion = idReservacion;
+    }
+
+    public Persona getHuesped() {
+        return huesped;
+    }
+
+    public void setHuesped(Persona huesped) {
+        this.huesped = huesped;
+    }
+
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public Precio getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Precio precio) {
+        this.precio = precio;
+    }
+
+    public Paquete getPaquete() {
+        return paquete;
+    }
+
+    public void setPaquete(Paquete paquete) {
+        this.paquete = paquete;
+    }
+
+    public Fecha getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Fecha fecha) {
+        this.fecha = fecha;
+    }
 
     public Reservacion(int NumDias) {
         this.NumDias = NumDias;
