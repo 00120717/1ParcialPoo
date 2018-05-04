b357c6b5edf91a48584d80721c63ae0ae4d404a9
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-05-04 07:59:14 -0600

    Se modifico el atributo paquete y se agregron los metodos seleccPaquete y mostrarPaquete

--- src/pkg1parcialpoo/Reservacion.java
+++ src/pkg1parcialpoo/Reservacion.java
@@ -5,6 +5,9 @@
  */
 package pkg1parcialpoo;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author R.Fuentes
@@ -14,9 +17,12 @@ public class Reservacion {
     public Persona huesped;
     public Habitacion habitacion;
     public Precio precio;
-    public Paquete paquete;
+    public String paquete;
     public int NumDias;
     public Fecha fecha;
+
+    public Reservacion() {
+    }
 
     public int getIdReservacion() {
         return idReservacion;
@@ -50,11 +56,11 @@ public class Reservacion {
         this.precio = precio;
     }
 
-    public Paquete getPaquete() {
+    public String getPaquete() {
         return paquete;
     }
 
-    public void setPaquete(Paquete paquete) {
+    public void setPaquete(String paquete) {
         this.paquete = paquete;
     }
 
@@ -83,8 +89,50 @@ public class Reservacion {
      precioFin=precio.getPrecioBase()*NumDias;
      return precioFin;
  }
- public int[][] hab = new int[6][10];
- 
+
+   public void MostrarPaquetes(){
+        System.out.println("Seleccione una opcion");
+        System.out.println("1. Basico");
+        System.out.println("2. Premium");
+        System.out.println("3. Ninguno");
+}
+ public void seleccPaquete(){
+          int opc=0;
+        Scanner leer= new Scanner(System.in);
+        while(opc!=4){
+            MostrarPaquetes();
+            try{
+                switch(opc){
+                    case 1:
+                        System.out.println("-Acceso a la picina."
+                                + "-Internet ilimitado.");
+                        setPaquete("basico");
+                        break;
+                    case 2:
+                        System.out.println("-Acceso al buffet de desayuno."
+                                + "-Acceso inlimitado a la picina."
+                                + "-Servicio a la habitacion."
+                                + "Acceso ilimitado al mini bar."
+                                + "Internet ilimitado.");
+                        setPaquete("Premium");
+                        break;
+                    case 3:
+                        System.out.println("Sin Paquete");
+                        setPaquete("Ninguno");
+                        break;
+                    default:
+                        System.out.println("Infrese una opcion valida.");
+                }
+            }
+            catch(InputMismatchException err){
+                System.err.println("Ingrese un numero");
+                leer.nextLine();
+            }
+        }
+        
+    }
+
+ public int[][] hab = new int[6][10]; 
  public void verificarHabitacion(){
       
          for(int i=0;i<7;i++){
