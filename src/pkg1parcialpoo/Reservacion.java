2a6dac8fb6e672761f9f6cd8520a57374efe2473
Marvin  Ramirez <Marvin  Ramirez@ZoniaGonzalez>
2018-05-07 14:36:35 -0600

    se agrego metodo precioEstadia y precioTotal

--- src/pkg1parcialpoo/Reservacion.java
+++ src/pkg1parcialpoo/Reservacion.java
@@ -20,7 +20,9 @@ public class Reservacion {
     public Paquete paquete;
     public int NumDias;
     public Fecha fecha;
-
+    public Fecha fechaSalida;
+    
+    
     public Reservacion() {
     }
 
@@ -56,11 +58,11 @@ public class Reservacion {
         this.precio = precio;
     }
 
-    public String getPaquete() {
+    public Paquete getPaquete() {
         return paquete;
     }
 
-    public void setPaquete(String paquete) {
+    public void setPaquete(Paquete paquete) {
         this.paquete = paquete;
     }
 
@@ -72,9 +74,7 @@ public class Reservacion {
         this.fecha = fecha;
     }
 
-    public Reservacion(int NumDias) {
-        this.NumDias = NumDias;
-    }
+    
 
     public int getNumDias() {
         return NumDias;
@@ -84,62 +84,33 @@ public class Reservacion {
         this.NumDias = NumDias;
     }
  
- public double precioTotal(){
+ public double precioEstadia(){
      double precioFin;
      precioFin=precio.getPrecioBase()*NumDias;
      return precioFin;
  }
-
-   public void MostrarPaquetes(){
-        System.out.println("Seleccione una opcion");
-        System.out.println("1. Basico");
-        System.out.println("2. Premium");
-        System.out.println("3. Ninguno");
-}
- public void seleccPaquete(){
-          int opc=0;
-        Scanner leer= new Scanner(System.in);
-        while(opc!=4){
-            MostrarPaquetes();
-            try{
-                switch(opc){
-                    case 1:
-                        System.out.println("-Acceso a la picina."
-                                + "-Internet ilimitado.");
-                        setPaquete("basico");
-                        break;
-                    case 2:
-                        System.out.println("-Acceso al buffet de desayuno."
-                                + "-Acceso inlimitado a la picina."
-                                + "-Servicio a la habitacion."
-                                + "Acceso ilimitado al mini bar."
-                                + "Internet ilimitado.");
-                        setPaquete("Premium");
-                        break;
-                    case 3:
-                        System.out.println("Sin Paquete");
-                        setPaquete("Ninguno");
-                        break;
-                    default:
-                        System.out.println("Infrese una opcion valida.");
-                }
-            }
-            catch(InputMismatchException err){
-                System.err.println("Ingrese un numero");
-                leer.nextLine();
-            }
-        }
-        
-    }
-
- public int[][] hab = new int[6][10]; 
- public void verificarHabitacion(){
-      
-         for(int i=0;i<7;i++){
-            for(int j=0;j<7;j++){
-                hab[i][j]=1;
-            }
  
- }}
+ public double precioTotal(){
+     double precioEstadia, precioPaquete=0;
+     precioEstadia=precio.getPrecioBase()*NumDias;
+     switch(paquete.paquete){
+         case 1: 
+             precioPaquete = paquete.precioBasico * NumDias;
+             break;
+        case 2: 
+             precioPaquete = paquete.precioPremium * NumDias;
+             break;
+        case 3: 
+             precioPaquete = 0;
+             break;
+        default:
+            System.out.println("");
+             
+     
+     }
+     return precioEstadia + precioPaquete;
+             
+             
+    }
 
 }
